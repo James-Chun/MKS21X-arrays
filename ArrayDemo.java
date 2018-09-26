c36d686eff41589ee641a4a075710bb756e60176
James-Chun <jchun00@stuy.edu>
2018-09-26 19:41:19 -0400

    main func added, still have no idea how to make it work

---
+++ ArrayDemo.java
@@ -0,0 +1,8 @@
+class ArrayDemo {
+    public static void main(String[] args){
+        public static void printArray(int[] ary){
+            int[] newArray = new int [ary.length];
+            System.out.println(newArray);
+        }
+    }
+}
