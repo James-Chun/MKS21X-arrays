7b4205fabda402a0930b0db90075df3d50bd9b98
James-Chun <jchun00@stuy.edu>
2018-09-27 21:09:21 -0400

    added 1

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,14 +1,27 @@
 class ArrayDemo {
     public static void main(String[] args) {
+        //0a test run
         int[] test = {1,2,3,4,5};
+        System.out.print("0a Test : ");
         printArray(test);
         
+        //0b test run
         int[][] test2 = {
             {6,7,8,9,10}, 
             {11,12,13,14,15}
         };
+        System.out.print("0b Test : ");
         printArray(test2);
+        
+        //1 test run
+        int[][] test3 = {
+            {1,0,2,0,0},
+            {3,0,4,0,5}
+        };
+        System.out.print("1  Test : ");
+        System.out.println (countZero2D(test3));
     }
+    
     //0a.
     public static void printArray(int[] ary){
         String returnValue = "[";
@@ -27,7 +40,19 @@ class ArrayDemo {
                 returnValue = returnValue + ary[i][i2] + ", " ;
             }
         }        
-        System.out.print (returnValue.substring(0,returnValue.length()-2) + "]");
+        System.out.println (returnValue.substring(0,returnValue.length()-2) + "]");
     }
     
+    //1.
+    public static int countZero2D(int[][] nums ) {
+        int zeroCounter = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            for (int i2 = 0; i2 < nums[i].length; i2 ++) {
+                if (nums[i][i2] == 0) {
+                    zeroCounter ++;
+                }
+            }
+        }  
+        return zeroCounter;
+    }
 }
