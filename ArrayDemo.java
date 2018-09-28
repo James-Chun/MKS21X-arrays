ce07cd6557b8af1e4ed734d0f8defbaf90b44eca
James-Chun <jchun00@stuy.edu>
2018-09-27 20:53:49 -0400

    made some more progress

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,11 +1,28 @@
 class ArrayDemo {
-    //0a.
     public static void main(String[] args) {
-        int[] a = {1,1,11,1};
-        printArray(a);
+        int[] test = {1,2,3,4,5};
+        printArray(test);
+        
+        int[][] test2 = {
+            {6,7,8,9,10}, 
+            {11,12,13,14,15}
+        };
+        //printArray(test2);
+        System.out.println (test2[0]);
+        System.out.println (test2[1]);
     }
+    //0a.
     public static void printArray(int[] ary){
-        int[] newArray = new int [ary.length];
-        System.out.println (newArray);
+        String returnValue = "[";
+        for (int i = 0; i < ary.length; i ++)
+        {
+            returnValue = returnValue + ary[i]+ ", " ;
+        }        
+        System.out.println (returnValue.substring(0,returnValue.length()-2) + "]");
     }
+    
+    //0b.
+    
+    
+    
 }
